ccb8533cf2c1e66ee78e20081d66a2ad6c99b566
dpdimitrova7 <dpdimitrova7@gmail.com>
2021-11-10 23:54:31 +0000

    added a test

---
+++ to-do-app/src/components/tasks-list/spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TasksList from './tasks-list.component';
+
+it('It should render without errors', () => {
+  expect(1).toBe(1);
+});
